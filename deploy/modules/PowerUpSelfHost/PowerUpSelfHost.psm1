e39bb47f365a9e5db88139e8d323cd22289e0fc6
Andrey Shchekin <ashmind@gmail.com>
2014-09-03 12:21:55 +1200

    SYMPHONY-1630 Improved logging around permission grant.

--- deploy/modules/PowerUpSelfHost/PowerUpSelfHost.psm1
+++ deploy/modules/PowerUpSelfHost/PowerUpSelfHost.psm1
@@ -5,6 +5,7 @@ function Grant-SelfHostUrl(
     [Parameter(Mandatory=$true)][string] $url,
     [Parameter(Mandatory=$true)][string] $user
 ) {
+    Write-Host "Granting permissions to host $url for user $user"
     netsh http add urlacl url="$url" user="$user"
     if ($LastExitCode -ne 0) {
         Write-Error "netsh failed, see previous messages for details."
