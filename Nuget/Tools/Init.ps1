c44cc87cb7fed3c9cc7bc5d0ab471bf469b751f0
Andrey Shchekin <ashmind@gmail.com>
2014-09-09 17:24:51 +1200

    Removed unneeded merge artifact.

--- Nuget/Tools/Init.ps1
+++
@@ -1,5 +0,0 @@
-param($installPath, $toolsPath, $package, $project)
-Set-StrictMode -Version 2
-
-Import-Module $installPath\PowerUp\Deploy\Modules\PowerUpFileSystem\PowerUpFileSystem.psm1
-Copy-Directory $installPath\PowerUp .\_powerup
