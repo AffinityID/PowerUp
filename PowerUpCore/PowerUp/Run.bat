6014f8055d18bb25c887d4178f1d43845c594dd5
Lee Lian Hoy <cruz@zyk3.net>
2014-09-10 17:50:58 +0800

    SYMPHONY-1632 Made operationProfile optional.

--- PowerUpCore/PowerUp/Run.bat
+++ PowerUpCore/PowerUp/Run.bat
@@ -6,7 +6,10 @@ if '%2' == '' goto USAGE
 :RUN
 	set policyCmd=$execPolicy = Get-ExecutionPolicy; if (!($execPolicy -eq 'Unrestricted')) { Set-ExecutionPolicy -ExecutionPolicy Unrestricted -Scope Process }
 
-	set execCmd=%~dp0RunPSake.ps1 -operation %1 -operationProfile %2
+	set execCmd=%~dp0RunPSake.ps1 -operation %1
+	if not '%2' == '' (
+		set execCmd=%execCmd% -operationProfile %2
+	)
 	if not '%3' == '' (
 		set execCmd=%execCmd% -task %3
 	)
@@ -16,5 +19,5 @@ if '%2' == '' goto USAGE
 
 :USAGE
 	echo Usage: 
-	echo 	Run.bat ^<operation^> ^<profile^> ^<task[optional]^>
+	echo 	Run.bat ^<operation^> ^<profile[optional]^> ^<task[optional]^>
 	exit /B
