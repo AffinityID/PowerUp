3b3a39a6bc2fd537af7ae7149ef6e496389e0da4
Lee Lian Hoy <cruz@zyk3.net>
2014-09-17 15:01:38 +0800

    SYMPHONY-1632 Slight change to simplify zip usage.

--- PowerUpCore/PowerUp/Modules/PowerUpZip/PowerUpZip.psm1
+++ PowerUpCore/PowerUp/Modules/PowerUpZip/PowerUpZip.psm1
@@ -4,11 +4,10 @@ $ErrorActionPreference = 'Stop'
 $zipExe = "$PSScriptRoot\7za.exe"
 
 function Compress-ZipFile(
-    [Parameter(Mandatory=$true)][string] $zipFileName,
     [Parameter(Mandatory=$true)][string] $sourcePath,
-    [string] $destinationPath = ".\"
+    [Parameter(Mandatory=$true)][string] $destinationPath,
 ) {
-    $cmd = "$zipExe a -r -tzip $destinationPath$zipFileName $sourcePath"
+    $cmd = "$zipExe a -r -tzip $destinationPath $sourcePath"
     Write-Host $cmd
     Invoke-Expression $cmd
 }
