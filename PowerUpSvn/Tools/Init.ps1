75b55456797f0e67e16db5ad4e3706a531ae0f72
Lee Lian Hoy <cruz@zyk3.net>
2014-09-18 18:05:41 +0800

    SYMPHONY-1661 Correction to PowerUpFileSystem path.

--- PowerUpSvn/Tools/Init.ps1
+++ PowerUpSvn/Tools/Init.ps1
@@ -1,5 +1,5 @@
 param($installPath, $toolsPath, $package, $project)
 Set-StrictMode -Version 2
 
-Import-Module $installPath\PowerUp\Modules\PowerUpFileSystem\PowerUpFileSystem.psm1
+Import-Module .\_powerup\Modules\PowerUpFileSystem\PowerUpFileSystem.psm1
 Copy-Directory $installPath\PowerUp .\_powerup
