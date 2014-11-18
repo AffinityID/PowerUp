0ed10f257d7b97855fdb6e41f3c3c92eb7e835f0
Andrey Shchekin <ashmind@gmail.com>
2014-11-19 11:10:11 +1300

    SYMPHONY-1832 Fixed NuGet excludes, removed nuget update -self for faster publish.

--- Publish.ps1
+++ Publish.ps1
@@ -13,14 +13,12 @@ $env:PSModulePath += ";.\PowerUpCore\PowerUp\Modules\"
 Import-Module PowerUpFileSystem
 Import-Module PowerUpNuGet
 
-Update-NuGet
-
 # Core package
 Copy-Directory .\PowerUpCore .\_output\PowerUpCore
-New-NuGetPackage ".\_output\PowerUpCore\Package.nuspec" ".\_output" "-Version $version -NoPackageAnalysis"
+New-NuGetPackage ".\_output\PowerUpCore\Package.nuspec" ".\_output" "-Version $version -NoPackageAnalysis -NoDefaultExcludes"
 
 # Svn package
 Copy-Directory .\PowerUpSvn .\_output\PowerUpSvn
-New-NuGetPackage ".\_output\PowerUpSvn\Package.nuspec" ".\_output" "-Version $version -NoPackageAnalysis"
+New-NuGetPackage ".\_output\PowerUpSvn\Package.nuspec" ".\_output" "-Version $version -NoPackageAnalysis -NoDefaultExcludes"
 
 Send-NuGetPackage ".\_output\*.nupkg" $server
