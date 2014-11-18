ee0e717594442d966fd7258b557d2397cc7da03e
Andrey Shchekin <ashmind@gmail.com>
2014-11-18 17:07:25 +1300

    SYMPHONY-1831 Deployment fix.

--- PowerUpCore/Tools/Init.ps1
+++ PowerUpCore/Tools/Init.ps1
@@ -2,9 +2,9 @@ param($installPath, $toolsPath, $package, $project)
 Set-StrictMode -Version 2
 
 Import-Module $installPath\PowerUp\Modules\PowerUpFileSystem\PowerUpFileSystem.psm1
-Invoke-Robocopy $installPath\PowerUp .\_powerup "/np /ns /ndl /njh /njs"
+Invoke-Robocopy $installPath\PowerUp .\_powerup "/S /np /ns /njh /njs"
 
 if (!(Test-Path ".\powerup.bat")) {
     # Test-Path is so that other files can be deleted if not needed
-    Invoke-Robocopy $installPath\RootFiles . "/xx /xc /xn /xo /np /ns /ndl /njh /njs"
+    Invoke-Robocopy $installPath\RootFiles . "/xx /xc /xn /xo /np /ns /njh /njs"
 }
