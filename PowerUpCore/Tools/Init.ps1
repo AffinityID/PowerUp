c97a033b02f2d1e0da998b452388f3141737a250
Andrey Shchekin <ashmind@gmail.com>
2014-11-19 09:58:52 +1300

    SYMPHONY-1832 Updated Init.ps1 to delete obsolete/extra files from _powerup, and reduce logging.

--- PowerUpCore/Tools/Init.ps1
+++ PowerUpCore/Tools/Init.ps1
@@ -2,7 +2,7 @@ param($installPath, $toolsPath, $package, $project)
 Set-StrictMode -Version 2
 
 Import-Module $installPath\PowerUp\Modules\PowerUpFileSystem\PowerUpFileSystem.psm1
-Invoke-Robocopy $installPath\PowerUp .\_powerup "/S /np /ns /njh /njs"
+Invoke-Robocopy $installPath\PowerUp .\_powerup "/s /purge /np /ns /njh /njs /ndl"
 
 if (!(Test-Path ".\powerup.bat")) {
     # Test-Path is so that other files can be deleted if not needed
