d967e42ce40ad89265205c55cff98df7225fd003
Andrey Shchekin <ashmind@gmail.com>
2014-11-28 15:23:28 +1300

    SYMPHONY-1871 Added better structure to PowerUpMeta output.

--- PowerUpCore/PowerUp/Modules/PowerUpMeta/PowerUpMeta.psm1
+++ PowerUpCore/PowerUp/Modules/PowerUpMeta/PowerUpMeta.psm1
@@ -7,11 +7,27 @@ function Invoke-PowerUp(
     [string] $profile = '',
     [string] $task = ''
 ) {
+    Import-Module PowerUpUtilities
+
+    $message = "$operation"
     $command = "$PSScriptRoot\..\..\Run -operation $operation"
     if ($buildNumber -ne 0) { $command += " -buildNumber $buildNumber" }
-    if ($profile -ne '')    { $command += " -operationProfile $profile" }
-    if ($task -ne '')       { $command += " -task $task" }
-    
-    Write-Host $command
-    Invoke-Expression $command
+    if ($profile -ne '')    {
+        $command += " -operationProfile $profile"
+        $message += " $profile"
+    }
+    if ($task -ne '')       { 
+        $command += " -task $task"
+        $message += " ($task)"
+    }
+
+    Write-Host "`r`n--- PowerUp $($message): starting  ---`r`n" -ForegroundColor White
+    try {
+        Invoke-External $command
+    }
+    catch {
+        Write-Host "`r`n--- PowerUp $($message): failed ---`r`n" -ForegroundColor Red
+        throw
+    }
+    Write-Host "`r`n--- PowerUp $($message): completed ---`r`n" -ForegroundColor White
 }
