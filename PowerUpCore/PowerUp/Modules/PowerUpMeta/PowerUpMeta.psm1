bc6523a32367d2116c5754d91245759a5306e867
Andrey Shchekin <ashmind@gmail.com>
2015-01-21 14:03:51 +1300

    SYMPHONY-1945 A set of minor refactorings and an improvement to Invoke-DatabaseMigrations that allows it to run in-process.

--- PowerUpCore/PowerUp/Modules/PowerUpMeta/PowerUpMeta.psm1
+++ PowerUpCore/PowerUp/Modules/PowerUpMeta/PowerUpMeta.psm1
@@ -1,7 +1,7 @@
 Set-StrictMode -Version 2
 $ErrorActionPreference = "Stop"
 
-function Invoke-PowerUp(
+function Get-PowerUpCommandAsString(
     [Parameter(Mandatory=$true)] [string] $operation,
     [int] $buildNumber = 0,
     [string] $profile = '',
@@ -11,15 +11,26 @@ function Invoke-PowerUp(
 
     $message = "$operation"
     $command = "$PSScriptRoot\..\..\Run -operation $operation"
-    if ($buildNumber -ne 0) { $command += " -buildNumber $buildNumber" }
-    if ($profile -ne '')    {
-        $command += " -operationProfile $profile"
-        $message += " $profile"
-    }
-    if ($task -ne '')       { 
-        $command += " -task $task"
-        $message += " ($task)"
-    }
+    if ($buildNumber -ne 0) { $command += " -buildNumber $buildNumber"  }
+    if ($profile -ne '')    { $command += " -operationProfile $profile" }
+    if ($task -ne '')       { $command += " -task $task"                }
+
+    return $command
+}
+
+function Invoke-PowerUp(
+    [Parameter(Mandatory=$true)] [string] $operation,
+    [int] $buildNumber = 0,
+    [string] $profile = '',
+    [string] $task = ''
+) {
+    Import-Module PowerUpUtilities
+
+    $message = "$operation"
+    if ($profile -ne '')    { $message += " $profile" }
+    if ($task -ne '')       { $message += " ($task)"  }
+    
+    $command = Get-PowerUpCommandAsString -Operation $operation -BuildNumber $buildNumber -Profile $profile -Task $task
 
     Write-Host "`r`n--- PowerUp $($message): starting  ---`r`n" -ForegroundColor White
     try {
