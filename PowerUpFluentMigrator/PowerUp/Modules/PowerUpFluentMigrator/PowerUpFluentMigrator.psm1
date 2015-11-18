200ea3ce8783c57c19b588ef281db49a0f5883ef
Andrey Shchekin <ashmind@gmail.com>
2015-11-18 13:12:20 +1300

    SYMPHONY-2933 Moved FluentMigrator support to a separate module and simplified the logic.

---
+++ PowerUpFluentMigrator/PowerUp/Modules/PowerUpFluentMigrator/PowerUpFluentMigrator.psm1
@@ -0,0 +1,38 @@
+function Invoke-FluentMigrator(
+    [Parameter(Mandatory=$true)][string] $toolsPath,
+    [Parameter(Mandatory=$true)][string] $assemblyPath,
+    [Parameter(Mandatory=$true)][string] $connectionString,
+    [string] $provider = 'SqlServer',
+    [string] $context,
+    [switch] $inProcess
+) {
+    Import-Module PowerUpUtilities
+    $assembly = [Reflection.Assembly]::LoadFrom((Get-Item $assemblyPath).FullName)
+    
+    if (!$inProcess) {
+        $migrate = "$toolsPath\Migrate.exe"
+    
+        $arguments = Format-ExternalArguments @{
+            '--assembly' = $assembly.Location
+            '--provider' = $provider
+            '--conn'     = $connectionString
+            '--context'  = $context
+        } -EscapeAll
+        Invoke-External "$migrate $arguments"
+    }
+    else {
+        Add-Type -Path "$toolsPath\FluentMigrator.dll"
+        Add-Type -Path "$toolsPath\FluentMigrator.Runner.dll"
+        
+        $announcer = New-Object FluentMigrator.Runner.Announcers.ConsoleAnnouncer
+        $runnerContext = New-Object FluentMigrator.Runner.Initialization.RunnerContext($announcer)
+        $runnerContext.Target = $assembly.Location
+        $runnerContext.Connection = $connectionString
+        $runnerContext.Database = $provider
+        $runnerContext.ApplicationContext = $context
+        $executor = New-Object FluentMigrator.Runner.Initialization.TaskExecutor($runnerContext)
+        $executor.Execute()
+    }
+}
+
+Export-ModuleMember -Function Invoke-FluentMigrator
