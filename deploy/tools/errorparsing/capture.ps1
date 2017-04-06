9f85cd932f4e05b06c9655756e2f9f8a6a00bf4f
Andrey Shchekin <ashmind@gmail.com>
2017-04-06 16:00:03 +1200

    Removed obsolete folder.

--- deploy/tools/errorparsing/capture.ps1
+++
@@ -1,8 +0,0 @@
-param(
-    [Parameter(Mandatory=$true)][String]$buildkey,
-    [Parameter(Mandatory=$true)][String]$buildnumber
-    )
-$buildkey > build-info.txt
-$buildnumber >> build-info.txt
-
-exit 0
